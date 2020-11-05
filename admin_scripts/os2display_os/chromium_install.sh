f7e9393f175234f387be0a9126f568b590d4f1d6
Carsten Agger <carstena@magenta.dk>
2020-11-05 10:58:57 +0100

    Made Chromium and minimal X work in 20.04.

--- admin_scripts/os2display_os/chromium_install.sh
+++ admin_scripts/os2display_os/chromium_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Minimal install of X and Chromium and connectivity.
-apt  install -y xinit chromium-browser
+apt-get install xinit xserver-xorg-core --no-install-recommends --no-install-suggests
+apt-get install chromium-browser
+
