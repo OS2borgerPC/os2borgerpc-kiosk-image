a4ff10a932615dc0b3213006fbbde3d5bcc9fd0d
Carsten Agger <carstena@magenta-aps.dk>
2020-11-06 14:53:39 +0100

    Fix Chromium installation so it doesn't cause job manager to crash.

--- admin_scripts/os2displaypc/chromium_install.sh
+++ admin_scripts/os2displaypc/chromium_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Minimal install of X and Chromium and connectivity.
 apt-get install -y xinit xserver-xorg-core --no-install-recommends --no-install-suggests
-apt-get install -y chromium-browser
+snap install chromium > /dev/null
 
