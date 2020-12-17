220c9cce3e138ab54124590135466fdbb03d3b16
Carsten Agger <carstena@magenta-aps.dk>
2020-12-17 14:08:10 +0100

    Fix dependencies (hopefully).

--- admin_scripts/os2displaypc/chromium_install.sh
+++ admin_scripts/os2displaypc/chromium_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Minimal install of X and Chromium and connectivity.
 apt-get install -y xinit xserver-xorg-core x11-xserver-utils --no-install-recommends --no-install-suggests
+apt-get install -y  xdg-utils xserver-xorg-video-qxl xserver-xorg-video-fbdev xserver-xorg-video-vesa xserver-xorg-video
 apt-get install -y chromium-browser > /dev/null
-
