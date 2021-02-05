e1e053fa569a0d1ec296d672ecf9467b4eef0d52
Carsten Agger <carstena@magenta-aps.dk>
2021-02-05 08:46:32 +0100

    Remember input stuff.

--- admin_scripts/os2displaypc/chromium_install.sh
+++ admin_scripts/os2displaypc/chromium_install.sh
@@ -2,5 +2,5 @@
 # Minimal install of X and Chromium and connectivity.
 
 apt-get install -y xinit xserver-xorg-core x11-xserver-utils --no-install-recommends --no-install-suggests
-apt-get install -y xdg-utils xserver-xorg-video-qxl xserver-xorg-video-intel xserver-xorg-video-all
+apt-get install -y xdg-utils xserver-xorg-video-qxl xserver-xorg-video-intel xserver-xorg-video-all xserver-xorg-input-all
 apt-get install -y chromium-browser > /dev/null
