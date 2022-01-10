a36c2e6626d15608622d427c7d1a0e19f7783da6
Marcus Funch Mogensen <mfm@magenta.dk>
2022-01-10 16:49:44 +0100

    Chromium install: Exit on all errors, apt-get update first, don't hide chromium installation output

--- admin_scripts/os2borgerpc_kiosk/chromium_install.sh
+++ admin_scripts/os2borgerpc_kiosk/chromium_install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Minimal install of X and Chromium and connectivity.
 
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update -y
+
 apt-get install -y xinit xserver-xorg-core x11-xserver-utils --no-install-recommends --no-install-suggests
 apt-get install -y xdg-utils xserver-xorg-video-qxl xserver-xorg-video-intel xserver-xorg-video-all xserver-xorg-input-all libleveldb-dev
-apt-get install -y chromium-browser > /dev/null
+apt-get install -y chromium-browser
