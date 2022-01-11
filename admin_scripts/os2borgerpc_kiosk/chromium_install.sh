a4b6dd4b4a357b971027fd7aedd84da9984781e5
Marcus Funch Mogensen <mfm@magenta.dk>
2022-01-11 16:16:55 +0100

    Restore > /dev/null redirect + explain why

--- admin_scripts/os2borgerpc_kiosk/chromium_install.sh
+++ admin_scripts/os2borgerpc_kiosk/chromium_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Minimal install of X and Chromium and connectivity.
 
-set -e
+set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -9,4 +9,7 @@ apt-get update -y
 
 apt-get install -y xinit xserver-xorg-core x11-xserver-utils --no-install-recommends --no-install-suggests
 apt-get install -y xdg-utils xserver-xorg-video-qxl xserver-xorg-video-intel xserver-xorg-video-all xserver-xorg-input-all libleveldb-dev
-apt-get install -y chromium-browser
+# Hvorfor > /dev/null?:
+# Chromium-install udskriver "scroll"-kommentarer for at have én linje, der bladrer forbi
+# i stedet for at fylde skærmen, og dette giver pt. ugyldig XML, når svaret sendes tilbage til serveren.
+apt-get install -y chromium-browser > /dev/null
