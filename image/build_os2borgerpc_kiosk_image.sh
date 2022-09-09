bb3707676a85d33d1facab29229cee2f5bf7e132
Marcus Funch <mfm@magenta.dk>
2022-09-09 14:15:28 +0200

    Late command seemingly fails to chmod superuser, so maybe it doesn't exist yet at that point. Moving it to os2borgerpc_kiosk_setup.

--- image/build_os2borgerpc_kiosk_image.sh
+++ image/build_os2borgerpc_kiosk_image.sh
@@ -8,7 +8,7 @@ pushd .
 
 if [[ -z $ISO_PATH || -z $IMAGE_NAME ]]
 then
-    echo -e "Usage: "$0" iso_file image_name [--clean]\n"
+    echo -e "Usage: $0 iso_file image_name [--clean]\n"
     echo "iso_file must be a valid path to the ISO file to be remastered"
     echo "image_name is the name of the output image"
     echo -e "clean: pass --clean to first delete temp build files, e.g. from within iso/\n"
@@ -33,10 +33,17 @@ build/extract_iso.sh "$ISO_PATH" iso
 cd iso/scripts/wifi || exit 1
 # These are downloaded from the host system currently, so maybe it needs to be built from a machine that's the same version of
 # Ubuntu as the target?
-# Note: If testing locally maybe comment out these two to make the build process a bit faster
+# Note: If testing locally consider commenting out these two to make the build process a bit faster
 sudo apt-get install --download-only --assume-yes network-manager language-pack-da
 sudo apt download $(tr '\n' ' ' < deps.txt)
 
 popd
 
-xorriso -as mkisofs -r   -V "$IMAGE_NAME"   -o "$IMAGE_NAME.iso"   -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot   -boot-load-size 4 -boot-info-table   -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot   -isohybrid-gpt-basdat -isohybrid-apm-hfsplus   -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin iso/boot iso
+xorriso -as mkisofs -r   -V "$IMAGE_NAME"   -o "$IMAGE_NAME.iso"   -J -l -b isolinux/isolinux.bin \
+    -c isolinux/boot.cat -no-emul-boot   -boot-load-size 4 -boot-info-table   -eltorito-alt-boot \
+    -e boot/grub/efi.img -no-emul-boot   -isohybrid-gpt-basdat -isohybrid-apm-hfsplus   \
+    -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin iso/boot iso
+
+if [ $? = 0 ]; then
+    echo "$IMAGE_NAME.iso was successfully created"
+fi
